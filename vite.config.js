896f6dced91e79af5eb79d8bce3c7a3ea9590d99
den7ee <ilucas_@live.com>
2023-01-24 23:28:06 +0000

    Fix the blank page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/den7ee.github.io/sf6-countdown/",
+  base: "/sf6-countdown/",
   plugins: [react()],
 })
