041a5f5c636ce51b52deabdbe69e33225514fb5b
den7ee <ilucas_@live.com>
2023-01-27 02:13:49 +0000

    Background and Fonts

---
+++ src/components/SF6Logo.jsx
@@ -0,0 +1,10 @@
+import './SF6Logo.css'
+import SF6LogoImg from '../assets/sf6logo.png'
+
+const SF6Logo = ({}) => {
+  return (
+    <img className='logo' src={SF6LogoImg} alt="Street Fighter 6 Logo"/>
+  )
+}
+
+export default SF6Logo
---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import SF6Logo from './SF6Logo'
+import './Title.css'
+
+const Counter = ({}) => {
+  return (
+    <h1 className='title'>
+        <SF6Logo />
+        <p>Launch Countdown</p>
+    </h1>
+  )
+}
+
+export default Counter
